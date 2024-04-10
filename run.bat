bea445c8efc1d7af0d357e146c07197455f91984
Rico Ardiansyah <161300198+Blane187@users.noreply.github.com>
2024-04-10 15:26:55 +0700

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+title HRVC Ai
+
+if not exist env (
+    echo Please run 'install.bat' first to set up the environment.
+    pause
+    exit /b 1
+)
+
+env\python.exe src/wrbui.py --open
+echo.
+pause
